a07e6845915068f689bf7dc0ca0747b3f306a28b
buketcankaya <bkt.can@gmail.com>
2023-06-11 21:09:12 +0300

    part1

---
+++ main.js
@@ -0,0 +1,27 @@
+
+// münü aç-kapa liste görünümü "|||"
+
+const btn=document.querySelector('.nav-btn');
+const nav=document.querySelector('nav');
+
+btn.addEventListener('click',() => {
+    nav.classList.toggle('toggle');
+});
+
+
+// videoları ekrana geldiği anda oynatma
+document.addEventListener('DOMContentLoaded', function () {
+    const videos = document.querySelectorAll('video');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.play();
+        } else {
+          entry.target.pause();
+        }
+      });
+    });
+    videos.forEach((video) => {
+      observer.observe(video);
+    });
+  });
